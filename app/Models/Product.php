ddc543f2640af9c97136562b1f5d957c7bd53b43
Luis Martinez <devluismartinezr@gmail.com>
2020-07-07 16:13:14 -0500

    Product Model and Status Model Created

---
+++ app/Models/Product.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use SoftDeletes;
+
+class Product extends Model{
+
+    protected $table = 'products';
+}
---
+++ app/Models/Status.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use SoftDeletes;
+
+class Status extends Model{
+    
+}

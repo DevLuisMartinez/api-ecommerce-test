c8541df348495f9dab8e6d06db72e2843d61ccda
Luis Martinez <devluismartinezr@gmail.com>
2020-07-07 23:14:08 -0500

    Product relationship many to many with cart

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use App\Models\Cart;
 use Illuminate\Database\Eloquent\Model;
 use SoftDeletes;
 
 class Product extends Model{
 
     protected $table = 'products';
+
+    public function carts(){
+        return $this->belongsToMany(Cart::class, 'products_carts');
+    }
 }

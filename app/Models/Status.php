5493ce63932728164018cc9b71522fa4fe872f29
Luis Martinez <devluismartinezr@gmail.com>
2020-07-07 23:13:41 -0500

    Status Relationship with cart

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Cart;
 use Illuminate\Database\Eloquent\Model;
 use SoftDeletes;
 
 class Status extends Model{
     
+    public function cart(){
+        
+        return $this->hasMany(Cart::class);
+    }
 }

fc50e945f981fc6f43eeae63e6b005f044bbf0b0
Luis Martinez <devluismartinezr@gmail.com>
2020-07-07 23:15:12 -0500

    Product Repository

---
+++ app/Repositories/Eloquent/ProductRepository.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace App\Repositories\Eloquent;
+
+use App\Models\Product;
+use App\Repositories\Eloquent\BaseRepository;
+use App\Repositories\ProductRepositoryInterface;
+
+class ProductRepository extends BaseRepository implements ProductRepositoryInterface {
+
+    public function __construct(Product $model){
+
+        parent::__construct( $model );
+    }
+
+    public function all(){
+
+        return $this->model->all();
+    }
+}
